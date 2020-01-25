2a942baee59e0e0294648eea3eda2fc6873294b7
Karine Cardoso <karine.cardoso_@hotmail.com>
2020-01-25 16:34:36 -0300

    Adiciona teste unitarios para Domain

---
+++ conta-corrente-api/tests/ContaCorrente.Domain.Tests/ContasCorrentes/ContaTests.cs
@@ -0,0 +1,32 @@
+﻿using ContaCorrente.Domain.ContasCorrentes;
+using FluentAssertions;
+using Xunit;
+
+namespace ContaCorrente.Domain.Tests.ContasCorrentes
+{
+    public class ContaTests
+    {
+        private readonly Conta _conta = new Conta
+        {
+            Id = 1,
+            Agencia = 1,
+            Numero = 12345,
+            Digito = 6,
+            Saldo = 100
+        };
+
+        [Fact]
+        public void Deve_retornar_true_quando_conta_corrente_com_saldo_suficiente()
+        {
+            _conta.TemSaldoSuficiente(99).Should().BeTrue();
+            _conta.TemSaldoSuficiente(100).Should().BeTrue();
+        }
+
+        [Fact]
+        public void Deve_retornar_false_quando_conta_corrente_com_saldo_insuficiente()
+        {
+            _conta.TemSaldoSuficiente(101).Should().BeFalse();
+            _conta.TemSaldoSuficiente(150).Should().BeFalse();
+        }
+    }
+}

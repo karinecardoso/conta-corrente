5d4b6675136c7ee8845b349806766b5dc0c811f6
Karine Cardoso <karine.cardoso_@hotmail.com>
2020-01-26 01:59:31 -0300

    Adiciona testes de integracao

---
+++ conta-corrente-api/tests/ContaCorrente.Api.Tests/Controllers/ContaControllerIntegrationTests.cs
@@ -0,0 +1,67 @@
+﻿using ContaCorrente.Api.Tests._Fixtures;
+using ContaCorrente.UseCases.Contracts.ContasCorrentes;
+using ContaCorrente.UseCases.Contracts.ContasCorrentes.Transacoes;
+using FluentAssertions;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ContaCorrente.Api.Tests.Controllers
+{
+    public class ContaControllerIntegrationTests : IClassFixture<ContaCorrenteWebApplicationFactory>
+    {
+        private readonly HttpClient _client;
+
+        public ContaControllerIntegrationTests(ContaCorrenteWebApplicationFactory factory)
+        {
+            _client = factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task Deve_obter_saldo_da_conta_corrente()
+        {
+            var httpResponseMessage = await _client.GetAsync("/conta/2/saldo");
+
+            httpResponseMessage.EnsureSuccessStatusCode();
+
+            var jsonFromPostResponse = await httpResponseMessage.Content.ReadAsStringAsync();
+            var dataResponse = JsonConvert.DeserializeObject<ContaDto>(jsonFromPostResponse);
+
+            dataResponse.Should().NotBeNull();
+        }
+
+        [Fact]
+        public async Task Deve_retornar_status_code_404_quando_conta_corrente_nao_existente_ao_obter_saldo()
+        {
+            var httpResponseMessage = await _client.GetAsync("/conta/4/saldo");
+
+            httpResponseMessage.StatusCode.Should().Be(HttpStatusCode.NotFound);
+        }
+
+        [Fact]
+        public async Task Deve_obter_extrato_da_conta_corrente()
+        {
+            var httpResponseMessage = await _client.GetAsync("/conta/2/extrato");
+
+            httpResponseMessage.EnsureSuccessStatusCode();
+
+            var jsonFromPostResponse = await httpResponseMessage.Content.ReadAsStringAsync();
+            var dataResponse = JsonConvert
+                .DeserializeObject<IReadOnlyCollection<TransacaoDto>>(jsonFromPostResponse);
+
+            dataResponse.Should().NotBeNull();
+            dataResponse.Should().HaveCountGreaterThan(0);
+        }
+
+        [Fact]
+        public async Task Deve_retornar_status_code_404_quando_conta_corrente_nao_existente_ao_obter_extrato()
+        {
+            var httpResponseMessage = await _client.GetAsync("/conta/4/extrato");
+
+            httpResponseMessage.StatusCode.Should().Be(HttpStatusCode.NotFound);
+        }
+    }
+}
---
+++ conta-corrente-api/tests/ContaCorrente.Api.Tests/Controllers/TransacaoControllerIntegrationTests.cs
@@ -0,0 +1,108 @@
+﻿using ContaCorrente.Api.Controllers.Requests;
+using ContaCorrente.Api.Tests._Fixtures;
+using FluentAssertions;
+using Newtonsoft.Json;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ContaCorrente.Api.Tests.Controllers
+{
+    public class TransacaoControllerIntegrationTests : IClassFixture<ContaCorrenteWebApplicationFactory>
+    {
+        private readonly HttpClient _client;
+
+        public TransacaoControllerIntegrationTests(ContaCorrenteWebApplicationFactory factory)
+        {
+            _client = factory.CreateClient();
+        }
+
+        private StringContent SerializeRequest(TransacaoRequest request)
+        {
+            var content = JsonConvert.SerializeObject(request);
+            return new StringContent(content, Encoding.UTF8, "application/json");
+        }
+
+        [Fact]
+        public async Task Deve_efetuar_deposito_na_conta_corrente()
+        {
+            var request = new TransacaoRequest { IdConta = 2, Valor = 100 };
+
+            var httpResponseMessage = await _client.PostAsync("/conta/deposito", SerializeRequest(request));
+
+            httpResponseMessage.StatusCode.Should().Be(HttpStatusCode.Accepted);
+        }
+
+        [Fact]
+        public async Task retornar_status_code_500_quando_conta_corrente_nao_existente_ao_efetuar_deposito()
+        {
+            var request = new TransacaoRequest { IdConta = 5, Valor = 100 };
+
+            var httpResponseMessage = await _client.PostAsync("/conta/deposito", SerializeRequest(request));
+
+            httpResponseMessage.StatusCode.Should().Be(HttpStatusCode.InternalServerError);
+        }
+
+        [Fact]
+        public async Task Deve_efetuar_retirada_na_conta_corrente()
+        {
+            var request = new TransacaoRequest { IdConta = 2, Valor = 100 };
+
+            var httpResponseMessage = await _client.PostAsync("/conta/retirada", SerializeRequest(request));
+
+            httpResponseMessage.StatusCode.Should().Be(HttpStatusCode.Accepted);
+        }
+
+        [Fact]
+        public async Task retornar_status_code_500_quando_conta_corrente_nao_existente_ao_efetuar_retirada()
+        {
+            var request = new TransacaoRequest { IdConta = 5, Valor = 100 };
+
+            var httpResponseMessage = await _client.PostAsync("/conta/retirada", SerializeRequest(request));
+
+            httpResponseMessage.StatusCode.Should().Be(HttpStatusCode.InternalServerError);
+        }
+
+        [Fact]
+        public async Task retornar_status_code_500_quando_saldo_insuficiente_ao_efetuar_retirada()
+        {
+            var request = new TransacaoRequest { IdConta = 2, Valor = 1000 };
+
+            var httpResponseMessage = await _client.PostAsync("/conta/retirada", SerializeRequest(request));
+
+            httpResponseMessage.StatusCode.Should().Be(HttpStatusCode.InternalServerError);
+        }
+
+        [Fact]
+        public async Task Deve_efetuar_pagamento_na_conta_corrente()
+        {
+            var request = new TransacaoRequest { IdConta = 3, Valor = 100 };
+
+            var httpResponseMessage = await _client.PostAsync("/conta/pagamento", SerializeRequest(request));
+
+            httpResponseMessage.StatusCode.Should().Be(HttpStatusCode.Accepted);
+        }
+
+        [Fact]
+        public async Task retornar_status_code_500_quando_conta_corrente_nao_existente_ao_efetuar_pagamento()
+        {
+            var request = new TransacaoRequest { IdConta = 5, Valor = 100 };
+
+            var httpResponseMessage = await _client.PostAsync("/conta/pagamento", SerializeRequest(request));
+
+            httpResponseMessage.StatusCode.Should().Be(HttpStatusCode.InternalServerError);
+        }
+
+        [Fact]
+        public async Task retornar_status_code_500_quando_saldo_insuficiente_ao_efetuar_pagamento()
+        {
+            var request = new TransacaoRequest { IdConta = 3, Valor = 1000 };
+
+            var httpResponseMessage = await _client.PostAsync("/conta/pagamento", SerializeRequest(request));
+
+            httpResponseMessage.StatusCode.Should().Be(HttpStatusCode.InternalServerError);
+        }
+    }
+}
